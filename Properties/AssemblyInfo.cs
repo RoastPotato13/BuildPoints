146dab544e89038e6edfbc2d5aff8108d2053ed8
RoastPotato <pmbarnes@umich.edu>
2026-09-22 19:37:48 -0500

    Update AssemblyInfo before release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("BuildPoints")]
 [assembly: AssemblyDescription("Adds a Build Points resource that gates craft construction, accrued over time based on career progress.")]
 [assembly: AssemblyProduct("BuildPoints")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
